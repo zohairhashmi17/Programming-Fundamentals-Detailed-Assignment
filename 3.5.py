cfd93b4cca33a0a496efb64e7f0fad39f7d35a04
zohairhashmi17 <45271874+zohairhashmi17@users.noreply.github.com>
2019-01-05 17:35:05 +0500

    Add files via upload

---
+++ 3.5.py
@@ -0,0 +1,7 @@
+print('zohair hashmi - 18b-127-cs - Section A')
+print('Practice Problem - 3.5')
+
+word =  ['blow','blown','glow','glown']
+for words in word:
+    if len(words)==4:
+        print(words)
