30032c4b21234970d891aa792b9964bed9de04d4
zohairhashmi17 <45271874+zohairhashmi17@users.noreply.github.com>
2019-01-05 15:44:16 +0500

    Add files via upload

---
+++ 3.1(a).py
@@ -0,0 +1,8 @@
+print('zohair hashmi - 18b-17-cs - Section A')
+print('Practice Problem - 3.1(a)')
+
+a = eval(input('enter a age:'))
+if a>62:
+    print('you can get your pension benefits')
+else:
+    print('We are sorry')
---
+++ 3.1(d).py
@@ -0,0 +1,9 @@
+print('zohair hashmi - 18b-17-cs - Section A')
+print('Practice Problem - 3.1(d)')
+
+a = input('enter a direction:')
+b = input('Enter a list:')
+if a in b:
+    print('I can escape')
+else:
+    print('I cannot escape')
