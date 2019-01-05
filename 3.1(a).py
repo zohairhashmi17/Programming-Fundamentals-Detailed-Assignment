9d611a4c9c1da58637e78b4a7b51ac28d63a4c96
zohairhashmi17 <45271874+zohairhashmi17@users.noreply.github.com>
2019-01-05 16:47:29 +0500

    Delete 3.1(a).py

--- 3.1(a).py
+++
@@ -1,8 +0,0 @@
-print('zohair hashmi - 18b-17-cs - Section A')
-print('Practice Problem - 3.1(a)')
-
-a = eval(input('enter a age:'))
-if a>62:
-    print('you can get your pension benefits')
-else:
-    print('We are sorry')
