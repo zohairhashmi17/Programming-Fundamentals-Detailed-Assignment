4caea678a5ef7ad0c90f44dce8d46f84381a827a
zohairhashmi17 <45271874+zohairhashmi17@users.noreply.github.com>
2019-01-05 16:53:19 +0500

    Delete 3.3(b).py

--- 3.3(b).py
+++
@@ -1,9 +0,0 @@
-print('zohair hashmi - 18b-17-cs - Section A')
-print('Practice Problem - 3.3b')
-
-ticket = input('enter a list:')
-lottery = input('enter a list:')
-if ticket==lottery:
-    print('You won!')
-else:
-    print('better luck next time')
